2e906841b0dd730cdea92dfa6c169db6aa335064
NdegeFelix <findech6@gmail.com>
2024-02-06 21:38:44 +0300

    updated validations

--- server/models.py
+++ server/models.py
@@ -1,17 +1,23 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
+
+
 db = SQLAlchemy()
 
 class Author(db.Model):
     __tablename__ = 'authors'
     
     id = db.Column(db.Integer, primary_key=True)
-    name= db.Column(db.String, unique=True, nullable=False)
+    name = db.Column(db.String, unique=True, nullable=False)
     phone_number = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators 
+    @validates('name')
+    def validate_name(self, key, name):
+        if len(name.strip()) < 2:
+            raise ValueError("Name must be at least 2 characters long")
+        return name
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -27,8 +33,11 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators  
-
+    @validates('title')
+    def validate_title(self, key, title):
+        if len(title.strip()) < 5:
+            raise ValueError("Title must be at least 5 characters long")
+        return title
 
     def __repr__(self):
-        return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
+        return f'Post(id={self.id}, title={self.title}, content={self.content}, summary={self.summary})'
